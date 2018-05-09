0d47eb2d42815d944c364d9b76fdbfc910f7eb79
gonzalo1398 <37145045+gonzalo1398@users.noreply.github.com>
2018-05-09 14:25:41 +0200

    Update TestMeeting.java

--- Homecoming/src/Test/TestMeeting.java
+++ Homecoming/src/Test/TestMeeting.java
@@ -5,10 +5,30 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestMeeting {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	
+	@After
+	public void testHourNotNULL () {
+		assertNotEquals (hour, nullHour);
 	}
-
-}
+		
+	@After
+	public void testPlaceNotNULL () {
+		assertNotEquals (place, nullString);
+	}
+		
+	@After
+	public void testDestinationNotNULL () {
+		assertNotEquals (destination, nullString);
+	}
+	
+	@Before
+	public void testHourFormat () {
+		assertTrue (hour.instanceof(timeStyle));
+	}
+	
+	@Before
+	public void testHourOutOfBounds () {
+		assertTrue (hour>=00:00:00);
+		assertTrue (hour<24:00:00);
+	}
+	
