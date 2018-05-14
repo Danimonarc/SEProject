bfb89bf69b4638614de95028c0e9e9e54228b6f1
AdriLaguna <34045675+AdriLaguna@users.noreply.github.com>
2018-05-14 10:06:37 +0200

    Update TestUser.java

--- Homecoming/src/Test/TestUser.java
+++ Homecoming/src/Test/TestUser.java
@@ -5,6 +5,17 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestUser {
+	
+	@Test
+	public void existentUser(User user){
+		bool expect=true, actual=false;
+		for(User n : userList){
+			if(user==n){
+				actual = true;
+			}
+		}
+		assertEquals("User already exists.", expect, actual);
+	}
 
 	@Test
 	public void test() {
