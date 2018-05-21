c85b7067ae48ecdda5ae66ca7859a148027dea51
Daniel Montero <32424377+Danimonarc@users.noreply.github.com>
2018-05-21 10:20:05 +0200

    Lo he arreglado

--- Homecoming/src/Test/TestGroup.java
+++ Homecoming/src/Test/TestGroup.java
@@ -1,14 +1,22 @@
-package Test;
-
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-
-public class TestGroup {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
-
-}
+ import static org.junit.Assert.*;
+ 
+-import java.util.*;
+-
+-import org.junit.*;
+-import org.junit.rules.ExpectedException;
++import org.junit.Test;
+ 
+ public class TestGroup {
+ 
+-	@Rule
+-	public ExpectedException e = ExpectedException.none();
+-	@Test public void getOnAnEmptyListThrowsException() {
+-		List<Object> l = new ArrayList<Object>();
+-		e.expect(IndexOutOfBoundsException.class);
+-		e.expectMessage("Index: 0, Size: 0");
+-		l.get(0); //Throws Exception
+-	}
+-	
+ 	@Test
+ 	public void test() {
+ 		fail("Not yet implemented");
