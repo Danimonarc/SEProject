3847528dad86437b86f26ace156bc4a934239872
azs98 <37145534+azs98@users.noreply.github.com>
2018-05-29 11:43:26 +0200

    Add files via upload

---
+++ Homecoming (Classes)/Group.java
@@ -0,0 +1,68 @@
+	import java.awt.Image;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Group {
+	private LocalTime hour;
+	private String place;
+	private String destination;
+	private List<User> members;
+	private Image map;
+	private List<Request> requests;
+	
+	private static final int MAX_MEMBERS = 20;
+	
+	public Group (LocalTime h, String p, String d, User creator) {
+		hour = h;
+		place = p;
+		destination = d;
+		members = new ArrayList(MAX_MEMBERS);  
+		members.add(creator);
+		requests = new ArrayList(MAX_MEMBERS);
+		
+		//map= SACAR DE LA BASE DE DATOS
+		
+	}
+	
+	public void setHour (LocalTime newhour) {
+		hour = newhour;
+	}
+
+	public String getPlace() {
+		return place;
+	}
+
+	public void setPlace(String place) {
+		this.place = place;
+	}
+
+	public LocalTime getHour() {
+		return hour;
+	}
+	
+	public void callPolice() {
+		//postureo
+	}
+	
+	public static int getMembers() {
+		return members;
+	}
+	
+	public void joinGroup (User newuser) {
+		members.add(newuser);
+	}
+	
+	public void leaveGroup (User olduser) {
+		members.remove((members.indexOf(olduser)));
+	}
+	
+	//PARA LA GUI: USAR PARA IMPRIMIR LOS MAPAS
+	public Image getMap () {
+		return map;
+	}
+	
+	public void acceptRequest(Request request) {
+		request.loquesea(members.size());
+	}
+}
